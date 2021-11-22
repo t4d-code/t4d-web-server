171e14740d7b5e78315220cfb990b539e9c170c1
Eric Greene <eric@t4d.io>
2021-11-21 20:01:33 -0500

    add cors

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 import { resolve } from 'path';
 import { Low, JSONFile } from 'lowdb';
 import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
 
 import auth from './plugins/auth.js';
 import api from './plugins/api.js';
@@ -24,6 +25,7 @@ export default async ({
 
     await database.read()
 
+    app.register(fastifyCors);
     app.register(auth({ expireAccessToken, expireRefreshToken }));
     app.register(api({ pluralModelName: 'colors', database }));
     app.register(api({ pluralModelName: 'cars', database }));
